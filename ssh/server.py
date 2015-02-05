98babc4a353f47f94c47cd6bacaeb6556a09c8a9
syndbg <syndbe@gmail.com>
2015-02-06 00:55:52 +0200

    Added SSH server

--- ssh/server.py
+++ ssh/server.py
@@ -1,55 +1,58 @@
+from os.path import expanduser
+
 from twisted.conch import avatar, recvline
+from twisted.conch.insults import insults
 from twisted.conch.interfaces import IConchUser, ISession
 from twisted.conch.ssh import factory, keys, session
-from twisted.conch.insults import insults
 from twisted.cred import portal, checkers
 from twisted.internet import reactor
 from zope.interface import implements
 
-class SSHDemoProtocol(recvline.HistoricRecvLine):
+
+class SSHProtocol(recvline.HistoricRecvLine):
+
     def __init__(self, user):
         self.user = user
 
     def connectionMade(self):
         recvline.HistoricRecvLine.connectionMade(self)
-        self.terminal.write("Welcome to my test SSH server.")
+        self.terminal.write('Welcome! {0}'.format(self.user.username))
         self.terminal.nextLine()
         self.do_help()
-        self.showPrompt()
+        self.show_prompt()
 
-    def showPrompt(self):
-        self.terminal.write("$ ")
+    def show_prompt(self):
+        self.terminal.write('$ ')
 
-    def getCommandFunc(self, cmd):
+    def get_command_function(self, cmd):
         return getattr(self, 'do_' + cmd, None)
 
     def lineReceived(self, line):
         line = line.strip()
         if line:
-            cmdAndArgs = line.split()
-            cmd = cmdAndArgs[0]
-            args = cmdAndArgs[1:]
-            func = self.getCommandFunc(cmd)
-            if func:
+            cmd_and_args = line.split()
+            cmd = cmd_and_args[0]
+            args = cmd_and_args[1:]
+            function = self.get_command_function(cmd)
+            if function:
                 try:
-                    func(*args)
-                except Exception, e:
-                    self.terminal.write("Error: %s" % e)
+                    function(*args)
+                except Exception as e:
+                    self.terminal.write('Error: {0}'.format(e))
                     self.terminal.nextLine()
             else:
-                self.terminal.write("No such command.")
+                self.terminal.write('No such command.')
                 self.terminal.nextLine()
-        self.showPrompt()
+        self.show_prompt()
 
     def do_help(self):
-        publicMethods = filter(
-            lambda funcname: funcname.startswith('do_'), dir(self))
-        commands = [cmd.replace('do_', '', 1) for cmd in publicMethods]
-        self.terminal.write("Commands: " + " ".join(commands))
+        public_methods = [function_name for function_name in dir(self) if function_name.startswith('do_')]
+        commands = [cmd.replace('do_', '', 1) for cmd in public_methods]
+        self.terminal.write('Commands: ' + ' '.join(commands))
         self.terminal.nextLine()
 
     def do_echo(self, *args):
-        self.terminal.write(" ".join(args))
+        self.terminal.write(' '.join(args))
         self.terminal.nextLine()
 
     def do_whoami(self):
@@ -57,14 +60,15 @@ class SSHDemoProtocol(recvline.HistoricRecvLine):
         self.terminal.nextLine()
 
     def do_quit(self):
-        self.terminal.write("Thanks for playing!")
+        self.terminal.write('Bye!')
         self.terminal.nextLine()
         self.terminal.loseConnection()
 
     def do_clear(self):
         self.terminal.reset()
 
-class SSHDemoAvatar(avatar.ConchUser):
+
+class SSHAvatar(avatar.ConchUser):
     implements(ISession)
 
     def __init__(self, username):
@@ -73,50 +77,53 @@ class SSHDemoAvatar(avatar.ConchUser):
         self.channelLookup.update({'session': session.SSHSession})
 
     def openShell(self, protocol):
-        serverProtocol = insults.ServerProtocol(SSHDemoProtocol, self)
-        serverProtocol.makeConnection(protocol)
-        protocol.makeConnection(session.wrapProtocol(serverProtocol))
+        server_protocol = insults.ServerProtocol(SSHProtocol, self)
+        server_protocol.makeConnection(protocol)
+        protocol.makeConnection(session.wrapProtocol(server_protocol))
 
-    def getPty(self, terminal, windowSize, attrs):
+    def getPty(self, terminal, window_size, attrs):
         return None
 
     def execCommand(self, protocol, cmd):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def closed(self):
         pass
 
-class SSHDemoRealm(object):
+
+class SSHRealm:
     implements(portal.IRealm)
 
     def requestAvatar(self, avatarId, mind, *interfaces):
-        if IConchUser in interfaces:
-            return interfaces[0], SSHDemoAvatar(avatarId), lambda: None
-        else:
-            raise NotImplementedError("No supported interfaces found.")
+        if IConchUser not in interfaces:
+            raise NotImplementedError('No supported interfaces found.')
+        return interfaces[0], SSHAvatar(avatarId), lambda: None
 
-def getRSAKeys():
-    with open('/home/syndbg/.ssh/id_rsa') as privateBlobFile:
-        privateBlob = privateBlobFile.read()
-        privateKey = keys.Key.fromString(data=privateBlob)
 
-    with open('/home/syndbg/.ssh/id_rsa.pub') as publicBlobFile:
-        publicBlob = publicBlobFile.read()
-        publicKey = keys.Key.fromString(data=publicBlob)
+def get_key(path):
+    with open(path) as f:
+        contents = f.read()
+    return keys.Key.fromString(data=contents)
 
-    return publicKey, privateKey
 
-if __name__ == "__main__":
-    sshFactory = factory.SSHFactory()
-    sshFactory.portal = portal.Portal(SSHDemoRealm())
+def get_RSA_keys():
+    ssh_folder = expanduser('~/.ssh/')
+    public_key = get_key(ssh_folder + 'id_rsa.pub')
+    private_key = get_key(ssh_folder + 'id_rsa')
+    return public_key, private_key
+
+
+if __name__ == '__main__':
+    factory = factory.SSHFactory()
+    factory.portal = portal.Portal(SSHRealm())
 
     users = {'admin': 'aaa', 'guest': 'bbb'}
-    sshFactory.portal.registerChecker(
+    factory.portal.registerChecker(
         checkers.InMemoryUsernamePasswordDatabaseDontUse(**users))
 
-    pubKey, privKey = getRSAKeys()
-    sshFactory.publicKeys = {'ssh-rsa': pubKey}
-    sshFactory.privateKeys = {'ssh-rsa': privKey}
+    pubKey, privKey = get_RSA_keys()
+    factory.publicKeys = {'ssh-rsa': pubKey}
+    factory.privateKeys = {'ssh-rsa': privKey}
 
-    reactor.listenTCP(2222, sshFactory)
+    reactor.listenTCP(2222, factory)
     reactor.run()
